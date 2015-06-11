232ba8f5aa4b07b542c7cdc4bde09179e1d9c5a8
Roy Rutto <royrutto@gmail.com>
2015-06-11 09:09:58 +0300

    RR: show calculate field answers on viewby

--- src/cljs/hatti/map/viewby.cljs
+++ src/cljs/hatti/map/viewby.cljs
@@ -52,6 +52,7 @@
    is a list of list of strings. For other types, a list of strings."
   (cond
    (f/select-one? field) raw-answers
+   (f/calculate? field) raw-answers
 ;   (f/numeric? field)    (evenly-spaced-bins raw-answers 5 "int")
 ;   (f/time-based? field) (evenly-spaced-bins raw-answers 5 "date")
    (f/select-all? field) (->> raw-answers
@@ -81,8 +82,9 @@
         ans-s (preprocess-answers field raw-answers)
         answer->count (frequencies (flatten ans-s))
         sorted (cond
-                (f/categorical? field) (map first
-                                            (sort-by second > answer->count))
+                 (or (f/categorical? field)
+                     (f/calculate? field)) (map first
+                                                (sort-by second > answer->count))
                 (or (f/time-based? field) (f/numeric? field)) (-> ans-s meta :bins))
         sorted-nil-at-end (move-nil-to-end sorted)
         colors (field->colors field)
