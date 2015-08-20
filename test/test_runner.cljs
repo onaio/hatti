d4ea3b1e5062134dce3a8534fd933c5f4ad3dfc9
Roy Rutto <royrutto@gmail.com>
2015-08-20 14:36:15 +0300

    RR: add test namespaces to test runner

--- test/test_runner.cljs
+++ test/test_runner.cljs
@@ -2,6 +2,14 @@
   (:require
    [cljs.test :as test :refer-macros [run-tests] :refer [report]]
    [hatti.test-utils]
+   [hatti.map.utils-test]
+   [hatti.map.viewby-test]
+   [hatti.ona.post-process-test]
+   [hatti.views.chart-test]
+   [hatti.views.dataview-test]
+   [hatti.views.map-test]
+   [hatti.views.record-test]
+   [hatti.views.table-test]
    [hatti.shared-test]))
 
 (enable-console-print!)
@@ -16,6 +24,15 @@
   (if (cljs.test/successful?
        (run-tests
         (test/empty-env ::test/default)
+        'hatti.test-utils
+        'hatti.map.utils-test
+        'hatti.map.viewby-test
+        'hatti.ona.post-process-test
+        'hatti.views.chart-test
+        'hatti.views.dataview-test
+        'hatti.views.map-test
+        'hatti.views.record-test
+        'hatti.views.table-test
         'hatti.shared-test))
    0
    1))
