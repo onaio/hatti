83e437cec238b5a06bd85aa18255cc32cb3af5b6
Roy Rutto <royrutto@gmail.com>
2015-08-28 15:48:55 +0300

    RR: try cljs externs

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                  [com.andrewmcveigh/cljs-time "0.2.3"]
                  ;; JS REQUIREMENTS
                  [cljsjs/moment "2.9.0-1"]
-                 [prabhasp/leaflet-cljs "0.7.3"]
+                 [cljsjs/leaflet "0.7.3-0"]
                  [cljsjs/jquery "1.9.1-0"]
                  [prabhasp/slickgrid-cljs "0.0.1"]
                  [prabhasp/osmtogeojson-cljs "2.2.5-1"]
