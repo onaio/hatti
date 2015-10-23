9ed351fae22acd8a47d6ff9e9c4f8110c0e2adca
pld <peetldee@gmail.com>
2015-10-23 16:02:51 -0700

    PLD: update api URL and remove unused helpers

--- src/hatti/ona/urls.cljs
+++ src/hatti/ona/urls.cljs
@@ -1,17 +1,7 @@
 (ns hatti.ona.urls
   (:require [hatti.utils :refer [url format]]))
 
-(def base-uri "http://ona.io/api/v1")
-
-(defn data-url [endpoint dataset-id & {:keys [:format] :or {:format "json"}}]
-  (url base-uri endpoint (str dataset-id "." format)))
-
-(defn formjson-url [dataset-id]
-  (url base-uri "forms" dataset-id "form.json"))
-
-(defn chart-url [dataset-id field-name]
-  (url base-uri "charts"
-       (str dataset-id ".json?field_name=" field-name)))
+(def base-uri "//api.ona.io/api/v1")
 
 (defn media-url [id fname]
   (url base-uri "files" (format "%s?filename=%s" id fname)))
