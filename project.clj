67ef05d86b3dd5fb0a1545573650b1111205df23
pld <peetldee@gmail.com>
2015-11-10 05:37:18 -0800

    PLD: bump to 0.1.12 snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onaio/hatti "0.1.11"
+(defproject onaio/hatti "0.1.12-SNAPSHOT"
   :description "A cljs dataview from your friends at Ona.io"
   :license "Apache 2, see LICENSE"
   :url "https://github.com/onaio/hatti"
