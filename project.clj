bc88ca59f0a7615be951b362bd7b934b45da144e
pld <peetldee@gmail.com>
2015-11-19 06:08:56 -0800

    PLD: bump to 0.1.14

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onaio/hatti "0.1.13"
+(defproject onaio/hatti "0.1.14"
   :description "A cljs dataview from your friends at Ona.io"
   :license "Apache 2, see LICENSE"
   :url "https://github.com/onaio/hatti"
