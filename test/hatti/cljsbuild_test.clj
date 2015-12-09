1aa1e7af08aceb61b77419996ae0b21a4fde353f
pld <peetldee@gmail.com>
2015-12-09 13:07:17 +0300

    PLD: this test is pushed directly into travis

--- test/hatti/cljsbuild_test.clj
+++
@@ -1,6 +0,0 @@
-(ns hatti.cljsbuild-test
-  (:require [clojure.java.shell :refer [sh]]
-            [midje.sweet :refer :all]))
-
-(fact "It shall combile the CLJS without error"
-      (re-find #"ERROR" (:out (sh "lein" "cljsbuild" "once" "prod"))) => nil)
