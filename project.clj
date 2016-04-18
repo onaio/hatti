b1251bda9b5befd5c73ff013508e4a00cd1e35cf
geoffreymuchai <geoffreymuchai@gmail.com>
2016-04-18 15:52:20 +0300

    GM: Change version to 0.1.19-submission-view-fix-SNAPSHOT

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
       [path]
       (str "resources/public/js/lib/" path))
 
-(defproject onaio/hatti "0.1.19-modal-submission-view-SNAPSHOT"
+(defproject onaio/hatti "0.1.19-submission-view-fix-SNAPSHOT"
   :description "A cljs dataview from your friends at Ona.io"
   :license "Apache 2, see LICENSE"
   :url "https://github.com/onaio/hatti"
