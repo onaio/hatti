42cc981ed4384159b08114909655b414583818e7
Okal Otieno <okal@justokal.com>
2016-06-24 11:40:38 +0300

    OO: Bump up to v0.2.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
       [path]
       (str "resources/public/js/lib/" path))
 
-(defproject onaio/hatti "0.1.24-SNAPSHOT"
+(defproject onaio/hatti "0.2.0"
   :description "A cljs dataview from your friends at Ona.io"
   :license "Apache 2, see LICENSE"
   :url "https://github.com/onaio/hatti"
