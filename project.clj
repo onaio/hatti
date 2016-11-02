2e2b38ba8248e9e7d850c5888cd2042283b61d36
Njagi Mwaniki <njagi@urbanslug.com>
2016-11-02 14:53:46 +0300

    Version bump to 0.3.6-tableview-videos-SNAPSHOT
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defn js-dir
-      "Prefix with full JavaScript directory."
-      [path]
-      (str "resources/public/js/lib/" path))
+  "Prefix with full JavaScript directory."
+  [path]
+  (str "resources/public/js/lib/" path))
 
-(defproject onaio/hatti "0.3.9-SNAPSHOT"
+
+(defproject onaio/hatti "0.3.6-tableview-videos-SNAPSHOT"
   :description "A cljs dataview from your friends at Ona.io"
   :license "Apache 2, see LICENSE"
   :url "https://github.com/onaio/hatti"
