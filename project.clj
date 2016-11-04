69caa694f259e05179fd239aa211f7541ca958b9
pld <peetldee@gmail.com>
2016-11-04 02:48:40 -0400

    PLD: bump to next hatti snapshot

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
       [path]
       (str "resources/public/js/lib/" path))
 
-(defproject onaio/hatti "0.3.8"
+(defproject onaio/hatti "0.3.9-SNAPSHOT"
   :description "A cljs dataview from your friends at Ona.io"
   :license "Apache 2, see LICENSE"
   :url "https://github.com/onaio/hatti"
