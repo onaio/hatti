3b31d82ac302cd11c8018c5f3d73b941fd5ba250
Njagi Mwaniki <njagi@urbanslug.com>
2016-11-07 12:43:21 +0300

    Version bump to 0.3.10
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   [path]
   (str "resources/public/js/lib/" path))
 
-(defproject onaio/hatti "0.3.9-tableview-videos-SNAPSHOT"
+(defproject onaio/hatti "0.3.10"
   :description "A cljs dataview from your friends at Ona.io"
   :license "Apache 2, see LICENSE"
   :url "https://github.com/onaio/hatti"
