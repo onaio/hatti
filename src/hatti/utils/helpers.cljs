d118f154263a728f5a9be217a159db1bcbab7bec
ivermac <mark.ekisa@gmail.com>
2017-04-26 12:05:14 +0300

    removed unnecessary helpers file

--- src/hatti/utils/helpers.cljs
+++
@@ -1,7 +0,0 @@
-(ns hatti.utils.helpers)
-
-(defn uses-search-expression?
-    "Checks if the appearance-value has the search expression"
-    [appearance-value]
-    (and (string? appearance-value)
-         (re-matches #"^search\(.*\)$" appearance-value)))
