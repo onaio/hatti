d8bf459eafa15be4af6d27bf8569173bd84adba3
Roy Rutto <royrutto@gmail.com>
2017-09-13 17:58:04 +0300

    bump hatti to 0.3.28

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   [path]
   (str "resources/public/js/lib/" path))
 
-(defproject onaio/hatti "0.3.28-SNAPSHOT"
+(defproject onaio/hatti "0.3.28"
   :description "A cljs dataview from your friends at Ona.io"
   :license "Apache 2, see LICENSE"
   :url "https://github.com/onaio/hatti"
