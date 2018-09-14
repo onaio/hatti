be2dae4243c96ee8becc850a112f33f142745ef9
ivermac <mark.ekisa@gmail.com>
2018-09-14 17:54:58 +0300

    Add constant variables and a utility function
    Signed-off-by: ivermac <mark.ekisa@gmail.com>

--- src/hatti/utils.cljs
+++ src/hatti/utils.cljs
@@ -58,3 +58,29 @@
                                          (render-static component)))
                                      components)]
     (join components-as-static-markup)))
+
+(def approved-text "Approved")
+(def approved-status "1")
+(def rejected-text "Rejected")
+(def rejected-status "2")
+(def pending-text "Pending")
+(def pending-status "3")
+
+(def review-status-map
+  "This hash-map contains the supported statuses that a form submission can be
+   assigned."
+  {approved-status approved-text
+   rejected-status rejected-text
+   pending-status pending-text})
+
+(def review-status-list
+  [{:review-status approved-status :review-text approved-text}
+   {:review-status rejected-status :review-text rejected-text}
+   {:review-status pending-status :review-text pending-text}])
+
+(defn get-submission-review-text
+  [row-map]
+  (get review-status-map
+       (if-let [r-status (get row-map "_review_status")]
+         r-status
+         pending-status)))
