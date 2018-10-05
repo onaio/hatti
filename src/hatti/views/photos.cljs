e56967f023616604e9d28069193f607e5ea098b0
John Mwashuma <jmwashuma@live.com>
2018-10-05 14:14:45 +0300

    Update externs file and add export key word

--- src/hatti/views/photos.cljs
+++ src/hatti/views/photos.cljs
@@ -47,7 +47,7 @@
     (format "%s <br/><small>Submitted at %s on %s</small>"
             (.-title item) time date)))
 
-(defn update-download-link-url
+(defn ^:export update-download-link-url
   [gallery]
   (js/console.log ">>I was here!!!!!")
   (js/console.log ">>gallery::" (clj->js gallery))
@@ -90,7 +90,7 @@
                                 js/PhotoSwipeUI_Default
                                 (clj->js photos)
                                 options)
-        _ (js/console.log ">>>up-gallery:" (clj->js gallery))
+        _ (js/console.log ">>>old-gallery:" (clj->js gallery))
         _ (js/console.log ">>>share-url:" (clj->js (-> gallery
                                                        .-options
                                                        .-getImageURLForShare)))]
@@ -101,7 +101,7 @@
     ;       (fn []
     ;         (js/console.log ">>Update Image url!!")
     ;         (or (aget gallery "currItem" "original-src") "")))
-    (js/console.log ">>>down-gallery:" (clj->js gallery))
+    (js/console.log ">>>new-gallery:" (clj->js gallery))
     (js/console.log ">>>share-url:" (clj->js (-> gallery
                                                  .-options
                                                  .-getImageURLForShare)))
